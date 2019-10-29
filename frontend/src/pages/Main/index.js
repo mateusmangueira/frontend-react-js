7f80607e1bcdbd5abc944956d315d28f66676ae6
Mateus Mangueira <mateus.mangueira@ccc.ufcg.edu.br>
2019-10-29 19:12:41 -0300

    Finalizando desafio

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 import api from '../../services/api';
+
 import Container from '../../components/Container';
 import { Form, SubmitButton, List } from './styles';
 
@@ -10,20 +12,19 @@ export default class Main extends Component {
     newRepo: '',
     repositories: [],
     loading: false,
+    error: null,
   };
 
-  // Para carregar os dados do localStorage
+  // Carregar os dados do localStorage
   componentDidMount() {
     const repositories = localStorage.getItem('repositories');
 
     if (repositories) {
-      this.setState({
-        repositories: JSON.parse(repositories)
-      });
-    };
-  };
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
 
-  // Para salvar os dados no localStorage
+  // Salvar os dados do localStorage
   componentDidUpdate(_, prevState) {
     const { repositories } = this.state;
 
@@ -33,27 +34,42 @@ export default class Main extends Component {
   }
 
   handleInputChange = e => {
-    this.setState({ newRepo: e.target.value });
-  }
+    this.setState({ newRepo: e.target.value, error: null });
+  };
 
   handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ loading: true });
+    this.setState({ loading: true, error: false });
 
-    const { newRepo, repositories } = this.state;
-    const response = await api.get(`/repos/${newRepo}`);
-    const data = { name: response.data.full_name };
+    try {
+      const { newRepo, repositories } = this.state;
 
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-      loading: false,
-    });
-  }
+      if (newRepo === '') throw 'Você precisa digitar um repositório';
+
+      const containsRepo = repositories.find(r => r.name === newRepo);
+
+      if (containsRepo) throw 'Repositório duplicado';
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+      });
+    } catch (error) {
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
 
   render() {
-    const { newRepo, repositories, loading } = this.state;
+    const { newRepo, repositories, loading, error } = this.state;
 
     return (
       <Container>
@@ -61,22 +77,31 @@ export default class Main extends Component {
           <FaGithubAlt />
           Repositórios
         </h1>
-        <Form onSubmit={this.handleSubmit}>
+
+        <Form onSubmit={this.handleSubmit} error={error}>
           <input
             type="text"
             placeholder="Adicionar repositório"
             value={newRepo}
             onChange={this.handleInputChange}
           />
-          <SubmitButton loading={loading} >
-            {loading ? <FaSpinner color="#FFF" size={14} /> : <FaPlus color="#FFF" size={14} />}
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+                <FaPlus color="#FFF" size={14} />
+              )}
           </SubmitButton>
         </Form>
+
         <List>
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
@@ -84,5 +109,3 @@ export default class Main extends Component {
     );
   }
 }
-
-
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Repository from './pages/Repository';
+
 export default function Routes() {
   return (
     <BrowserRouter>
